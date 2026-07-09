42d7fac5144e46f93b53368dba9d221b98022ac6
Zahil Akram Marankularanga <zahilakramofficial@gmail.com>
2026-07-09 15:05:05 +0530

    Add CodeChef string problems

---
+++ CodeChef/Practice/Strings/Valid Anagram.py
@@ -0,0 +1,38 @@
+# def isAnagram(s, t):
+#     lst=list(s)
+#     for i in t:
+#         if i in lst:
+#             lst.remove(i)
+        
+#         else:
+#             return 0
+#     return 1
+def isAnagram(s, t):
+    freq_s={}
+    freq_t={}
+
+    for i in s:
+        if i in freq_s:
+            freq_s[i]+=1
+            
+        else:
+            freq_s[i]=1
+            
+    for i in t:
+        if i in freq_t:
+            freq_t[i]+=1
+            
+        else:
+            freq_t[i]=1
+            
+    if freq_s==freq_t:
+        return 1
+    else:
+        return 0
+        
+# def isAnagram(s, t):
+#     if sorted(s)==sorted(t):
+#         return 1
+#     else:
+#         return 0
+    
