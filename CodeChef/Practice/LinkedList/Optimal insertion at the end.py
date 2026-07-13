5fea56c98b43caa78f9dc68267cb01850dd3853c
Zahil Akram Marankularanga <zahilakramofficial@gmail.com>
2026-07-13 20:43:29 +0530

    Add CodeChef linkedlist problem

---
+++ CodeChef/Practice/LinkedList/Optimal insertion at the end.py
@@ -0,0 +1,19 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+        self.tail = None
+
+    def insert_at_end(self, value):
+        new_node = Node(value)
+        
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        else:
+            current=self.tail
+            current.next=new_node
+            self.tail=new_node
+        
+     
